b3a630f185d58494f92037bd8ba53768ca57e10a
Christy Rollinson <crollinson@gmail.com>
2020-03-06 16:17:29 -0600

    New summary figures; saved to Google drive

--- Understory_Veg/scripts/01_Explore_UnderstoryVeg.R
+++ Understory_Veg/scripts/01_Explore_UnderstoryVeg.R
@@ -2,21 +2,32 @@
 library(ggplot2)
 source("0_Format_Understory_Data.R")
 
+path.google <- "/Volumes/GoogleDrive/My Drive/East Woods/Rollinson_Monitoring/Data/Understory_Vegetation/"
+path.figs <- file.path(path.google, "figures")
+dir.create(path.figs, recursive = T, showWarnings = F)
+
 # Load the 2019 Understory plot data from 
 dat.2019 <- get.understory(YEAR=2019)
 dat.2019 <- dat.2019[!dat.2019$Name.Common=="NOTHING",]
 summary(dat.2019)
 unique(dat.2019$Name.Common)
 
-ggplot(data=dat.2019) +
-  facet_wrap(~Plot) +
-  geom_histogram(aes(x=Obs.Date, fill=Subplot), stat="count", binwidth=8) +
-  scale_y_continuous(name="Number Species")
+plot.summary <- aggregate(Cover ~ Plot + Subplot + Obs.Date, data=dat.2019, FUN=sum)
+plot.summary$Richness <- aggregate(Cover ~ Plot + Subplot + Obs.Date, data=dat.2019, FUN=length)$Cover
+summary(plot.summary)
 
-ggplot(data=dat.2019) +
+png(file.path(path.figs, "UnderstoryVegetation_2019_TotalCover.png"), height=6, width=6, units="in", res=120)
+ggplot(data=plot.summary) +
   facet_wrap(~Plot) +
-  geom_histogram(aes(x=Obs.Date, y=Cover/4, fill=Subplot), stat="sum", binwidth=8) +
-  scale_y_continuous(name="Total Cover")
+  geom_boxplot(aes(x=Obs.Date, y=Cover, group=Obs.Date))
+dev.off()
+
+png(file.path(path.figs, "UnderstoryVegetation_2019_SpeciesRichness.png"), height=6, width=6, units="in", res=120)
+ggplot(data=plot.summary) +
+  facet_wrap(~Plot) +
+  geom_boxplot(aes(x=Obs.Date, y=Richness, group=Obs.Date))
+dev.off()
+
 
 
 # get the data from a particular sheet
